211766eb1fe24a75d8ca7413db7eb9ec8698896c
pottsie <pottsma@me.com>
2022-12-08 20:36:45 -0500

    Initial Commit

---
+++ FootballStats2022/FootballStats2022App.swift
@@ -0,0 +1,17 @@
+//
+//  FootballStats2022App.swift
+//  FootballStats2022
+//
+//  Created by Michael Potts on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct FootballStats2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
